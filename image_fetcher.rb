122cdd21edb61d0c3f7b7cc5c5f6ccea6919a318
EmilyPlatzer <emilyplatzer@gmail.com>
2014-05-05 09:25:33 -0600

    tests pass. Return 25 children from data set, return data for first child,
    return an array with all images, return an array of all IMGUR images.

--- image_fetcher.rb
+++ image_fetcher.rb
@@ -7,15 +7,28 @@ class ImageFetcher
   end
 
   def count_children
+    @data['data']['children'].size
   end
 
   def first_child
+    @data['data']['children'].first['data']
   end
 
   def all_images
+    all_images = []
+    @data['data']['children'].each do |child|
+      all_images << child['data']['url']
+    end
+    all_images
   end
 
   # fetch only the images that begin with `http://i.imgur.com`
   def only_imgur_images
+    all_images = []
+    @data['data']['children'].each do |child|
+      url=child['data']['url']
+      all_images << url if url.slice(0..17) == 'http://i.imgur.com'
+    end
+    all_images
   end
 end
